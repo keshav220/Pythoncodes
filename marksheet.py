f78edba776af282527799a374cea0bc47da9d74c
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-04-29 15:48:33 +0530

    more work

---
+++ marksheet.py
@@ -0,0 +1,27 @@
+   n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_scores = student_marks[query_name]
+    print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
+
+'''3
+Krishna 67 68 69
+Arjun 70 98 63
+Malika 52 56 60
+Malika'''
+
+#output = 56
+'''  n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    query_scores = student_marks[query_name]
+    print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))'''
